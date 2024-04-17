bd606ac9f0d03f367457b1bb996a4c8ce0fb84b3
Yash <yashsolanki0102@gmail.com>
2024-04-17 01:02:07 -0700

    Working NewUserLogin BUTTON AND AdminLogin BUTTON

---
+++ app/src/main/java/com/example/khatta/NewUserLogin.java
@@ -0,0 +1,13 @@
+package com.example.khatta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewUserLogin extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_user_login);
+    }
+
+}
