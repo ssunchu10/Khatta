8ba5beab481e289dd06db4fe26260aedf60f80bd
Yash <yashsolanki0102@gmail.com>
2024-04-17 14:34:08 -0700

    intent factory is working properly

--- app/src/main/java/com/example/khatta/NewUserLogin.java
+++ app/src/main/java/com/example/khatta/NewUserLogin.java
@@ -1,13 +1,31 @@
 package com.example.khatta;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewUserLogin extends Activity {
+    EditText newUsername, newPassword, confirmPassword;
+    Button createAccountButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_user_login);
+        createAccountButton = findViewById(R.id.create_account_button);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = newUsername.getText().toString().trim();
+                String password = newPassword.getText().toString().trim();
+                String confirm = confirmPassword.getText().toString().trim();
+                Intent intent = new Intent(NewUserLogin.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
