8b0efe89d66b1cfc3e777eb6b91693f468ff10f9
Yash <yashsolanki0102@gmail.com>
2024-04-16 23:20:30 -0700

    New Project

--- app/src/androidTest/java/com/example/khatta/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/khatta/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.khattacom", appContext.getPackageName());
+        assertEquals("com.example.khatta", appContext.getPackageName());
     }
 }
