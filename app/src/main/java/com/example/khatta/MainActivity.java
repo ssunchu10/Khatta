ecb328156f75f3a431f99c14ac2d19c471d9dee6
Yash <yashsolanki0102@gmail.com>
2024-04-19 00:02:43 -0700

    Landing page connected

--- app/src/main/java/com/example/khatta/MainActivity.java
+++ app/src/main/java/com/example/khatta/MainActivity.java
@@ -1,32 +1,39 @@
 package com.example.khatta;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.khatta.database.AppDatabase;
+import com.example.khatta.database.DatabaseInitializer;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private AppDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button loginButton = findViewById(R.id.loginButton);
-        Button signupButton = findViewById(R.id.signupButton);
+        Button alreadyhaveanaccount = findViewById(R.id.already_have_an_account_button);
+        Button createaccount = findViewById(R.id.create_account_button);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Login.class);
-                startActivity(intent);
-            }
-        });
-        signupButton.setOnClickListener(new View.OnClickListener() {
+        database = DatabaseInitializer.getInstance(this).getDatabase();//chaange it later
+        createaccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, NewUserLogin.class);
+                startActivity(intent);
+            }
+        });
+        alreadyhaveanaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
                 startActivity(intent);
             }
         });
