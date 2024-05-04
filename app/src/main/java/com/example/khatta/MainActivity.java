ce7e2c051732b99f118a3ba1abe127d12ff59e22
Yash <yashsolanki0102@gmail.com>
2024-05-04 16:06:12 -0700

    navigation bar

--- app/src/main/java/com/example/khatta/MainActivity.java
+++ app/src/main/java/com/example/khatta/MainActivity.java
@@ -2,17 +2,29 @@ package com.example.khatta;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.khatta.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         Button login = findViewById(R.id.already_have_an_account_button);
         Button createAccount = findViewById(R.id.create_account_button);
@@ -32,5 +44,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
---
+++ app/src/main/java/com/example/khatta/bottomBar.java
@@ -0,0 +1,54 @@
+package com.example.khatta;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.khatta.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class bottomBar extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        bottomNavigationView = findViewById(R.id.NavigationBar);
+        replaceFragment(new addexpenseFragment());
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.add:
+                        replaceFragment(new addexpenseFragment());
+                        break;
+                    case R.id.activity:
+                        replaceFragment(new activityFragment());
+                        break;
+                    case R.id.logout:
+                        replaceFragment(new logoutFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.FrameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
+
